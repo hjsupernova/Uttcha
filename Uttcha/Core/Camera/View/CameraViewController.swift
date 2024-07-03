c2251b8407cbf640cbced602117ca0340284d330
hjsupernova <kem08120506@gmail.com>
2024-07-03 16:20:02 +0900

    fix: CameraViewController의 capture session이 멈추지 않는 버그 해결 CameraView가 백그라운드로 가거나 내비게이션 이동 시 바로 멈추지 않는 버그가 있어 sessionResult enum과 viewWillDisappear을 활용해 뷰가 session을 멈추도록 했습니다.

--- Uttcha/Core/Camera/View/CameraViewController.swift
+++ Uttcha/Core/Camera/View/CameraViewController.swift
@@ -11,57 +11,185 @@ import AVFoundation
 import SwiftUI
 
 class CameraViewController: UIViewController {
-    var faceDetector: FaceDetector?
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    let session = AVCaptureSession()
-    private let stillImageOutput = AVCapturePhotoOutput()
+    // MARK: View Controller Life Cycle
 
-    let videoOutputQueue = DispatchSerialQueue(
-        label: "VideoOutputQueue",
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            break
+        case .notDetermined:
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if !granted {
+                    self.setupResult = .notAuthorized
+                }
+                self.sessionQueue.resume()
+            }
+        default:
+            setupResult = .notAuthorized
+        }
+        
+        /*
+         Setup the capture session.
+         In general, it's not safe to mutate an AVCaptureSession or any of its
+         inputs, outputs, or connections from multiple threads at the same time.
+
+         Don't perform these tasks on the main queue because
+         AVCaptureSession.startRunning() is a blocking call, which can
+         take a long time. Dispatch session setup to the sessionQueue, so
+         that the main queue isn't blocked, which keeps the UI responsive.
+         */
+        sessionQueue.async {
+            self.configureCaptureSession()
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        sessionQueue.async {
+            switch self.setupResult {
+            case .success:
+                // Only start the session if setup succeeded.
+                self.session.startRunning()
+                self.isSessionRunning = self.session.isRunning
+            case .notAuthorized:
+                DispatchQueue.main.async {
+                    let changePrivacySetting = "앱에 카메라 사용 권한이 없습니다. 설정을 변경해주세요."
+                    let message = NSLocalizedString(
+                        changePrivacySetting,
+                        comment: "Alert message when the user has denied access to the camera"
+                    )
+                    let alertController = UIAlertController(title: "Uttcha", message: message, preferredStyle: .alert)
+
+                    alertController.addAction(UIAlertAction(title: NSLocalizedString("취소", comment: "Alert OK button"),
+                                                            style: .cancel,
+                                                            handler: nil))
+
+                    alertController.addAction(UIAlertAction(title: NSLocalizedString("설정으로 이동",
+                                                                                     comment: "Alert button to open Settings"),
+                                                            style: .`default`,
+                                                            handler: { _ in
+                                                                UIApplication.shared.open(
+                                                                    URL(string: UIApplication.openSettingsURLString)!,
+                                                                    options: [:],
+                                                                    completionHandler: nil)
+                    }))
+
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            case .configurationFailed:
+                DispatchQueue.main.async {
+                    let alertMsg = "Alert message when something goes wrong during capture session configuration"
+                    let message = NSLocalizedString("카메라 사용이 불가능합니다.", comment: alertMsg)
+                    let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
+
+                    alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"),
+                                                            style: .cancel,
+                                                            handler: nil))
+
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        sessionQueue.async {
+            if self.setupResult == .success {
+                self.session.stopRunning()
+                self.isSessionRunning = self.session.isRunning
+            }
+        }
+
+        super.viewWillDisappear(animated)
+    }
+
+    // MARK: - Session Management
+
+    private enum SessionSetupResult {
+        case success
+        case notAuthorized
+        case configurationFailed
+    }
+
+    private let session = AVCaptureSession()
+    private var isSessionRunning = false
+
+    private let sessionQueue = DispatchSerialQueue(
+        label: "session queue",
         qos: .userInitiated,
         attributes: [],
         autoreleaseFrequency: .workItem
     )
 
-    override func viewDidLoad() {
-        configureCaptureSession()
-        setupSaveToCameraRoll()
-        session.startRunning()
-    }
+    private var setupResult: SessionSetupResult = .success
+    
+    private var previewLayer: AVCaptureVideoPreviewLayer?
 
+    // Call this on the session queue.
     private func configureCaptureSession() {
-        guard let camera = AVCaptureDevice.default(
-            .builtInWideAngleCamera,
-            for: .video,
-            position: .front
-        ) else {
-            // TODO: Error Handling
+        if setupResult != .success {
             return
         }
 
-        do {
-            let cameraInput = try AVCaptureDeviceInput(device: camera)
+        session.beginConfiguration()
 
-            if session.canAddInput(cameraInput) {
-                session.addInput(cameraInput)
+        // Add video input
+        do {
+            guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+                setupResult = .configurationFailed
+                session.commitConfiguration()
+                return
+            }
+
+            let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+
+            if session.canAddInput(videoDeviceInput) {
+                session.addInput(videoDeviceInput)
+
+                DispatchQueue.main.async {
+                    self.previewSetup()
+                }
+            } else {
+                print("Couldn't add video device input to the session.")
+                setupResult = .configurationFailed
+                session.commitConfiguration()
+                return
             }
         } catch {
-            // TODO: Error
+            print("Couldn't create video device input: \(error)")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
             return
         }
 
+        // Add an video data output
         let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.alwaysDiscardsLateVideoFrames = true
-        videoOutput.setSampleBufferDelegate(faceDetector, queue: videoOutputQueue)
-        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
 
         if session.canAddOutput(videoOutput) {
             session.addOutput(videoOutput)
+
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            videoOutput.setSampleBufferDelegate(faceDetector, queue: sessionQueue)
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+
+            if let connection = videoOutput.connection(with: .video) {
+                connection.videoOrientation = .portrait
+            }
+        } else {
+            print("Could not add video data output to the session")
+            setupResult = .configurationFailed
+            session.commitConfiguration()
+            return
         }
 
-        let videoConnection = videoOutput.connection(with: .video)
-        videoConnection?.videoOrientation = .portrait
+        session.commitConfiguration()
+    }
 
+    private func previewSetup() {
         previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer?.videoGravity = .resizeAspect
         previewLayer?.frame = view.bounds
@@ -71,9 +199,5 @@ class CameraViewController: UIViewController {
         }
     }
 
-    private func setupSaveToCameraRoll() {
-        if session.canAddOutput(stillImageOutput) {
-            session.addOutput(stillImageOutput)
-        }
-    }
+    var faceDetector: FaceDetector?
 }
