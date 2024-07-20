1eecfee7676b8703cddd4ecd9640f1528c363465
hjsupernova <kem08120506@gmail.com>
2024-07-20 16:50:38 +0900

    feat: SpotifyController accessToken 가져오도록 코드 작성 및 뷰 환경에 주입

--- Uttcha/App/UttchaApp.swift
+++ Uttcha/App/UttchaApp.swift
@@ -10,16 +10,24 @@ import SwiftUI
 
 @main
 struct UttchaApp: App {
-    private var coreDataStack = CoreDataStack.shared
+    @StateObject var spotifyController = SpotifyController()
     @AppStorage(UserDefaultsKeys.isNotificationOn) var isNotificationOn = false
     @AppStorage(UserDefaultsKeys.selectedTimeOption) var selectedTimeOption = NotificationTimeOption.day
+
+    private var coreDataStack = CoreDataStack.shared
 
     var body: some Scene {
         WindowGroup {
             UttchaTapView()
+                .preferredColorScheme(.dark)
+                .tint(.white)
                 .environment(\.managedObjectContext, coreDataStack.persistentContainer.viewContext)
+                .environmentObject(spotifyController)
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
                     coreDataStack.save()
+                }
+                .onOpenURL { url in
+                    spotifyController.setAccessToken(from: url)
                 }
                 .onAppear {
                     if isNotificationOn {
@@ -31,8 +39,6 @@ struct UttchaApp: App {
                     }
 
                 }
-                .preferredColorScheme(.dark)
-                .tint(.white)
         }
     }
 }
