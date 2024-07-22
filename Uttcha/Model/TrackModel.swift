f7bdc1ad01cad520778abe0d00510a2ed01b1777
hjsupernova <kem08120506@gmail.com>
2024-07-22 19:01:45 +0900

    feat: TrackModel 생성

---
+++ Uttcha/Model/TrackModel.swift
@@ -0,0 +1,17 @@
+//
+//  MusicModel.swift
+//  Uttcha
+//
+//  Created by KHJ on 7/22/24.
+//
+
+import Foundation
+
+struct TrackModel: Identifiable {
+    let id: UUID
+    let trackURI: String
+    let trackName: String
+    let trackArtist: String
+    let trackImage: Data? 
+    let dateCreated: Date
+}
